a75e338a9e6152c3da917bbf9a45dc77e9ad11d5
Zafar205 <mz09206@st.habib.edu.pk>
2025-07-17 00:54:33 +0500

    another patch

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: false,
-  distDir: 'build',
   eslint: {
     // Disable ESLint during builds
     ignoreDuringBuilds: true,
